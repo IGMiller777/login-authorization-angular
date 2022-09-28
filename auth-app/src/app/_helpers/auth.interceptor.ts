7c56d721cf376df7ce267e018ca0368269a2b874
Ivan <igamanovich@elinext.com>
2022-09-28 19:26:06 +0300

    feat: add interceptor config

--- auth-app/src/app/_helpers/auth.interceptor.ts
+++ auth-app/src/app/_helpers/auth.interceptor.ts
@@ -5,7 +5,8 @@ import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
 import { TokenStorageService } from '../_services/token-storage.service';
 import { Observable } from 'rxjs';
 
-const TOKEN_HEADER_KEY = 'Authorization';
+const TOKEN_HEADER_KEY = 'x-access-token';
+// const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
